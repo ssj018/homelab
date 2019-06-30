a9842ee17a5dc8e50f4da29ce91f26cf933e1229
shuangjian <659403872@qq.com>
2019-06-30 13:46:06 +0800

    delete cmdb_excel

--- cmdb_excel/cmdb/w.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-cmd='cd /opt/cmdb/;ansible-cmdb -C cols/cust_ser.conf out_ser/ '
-
-output = subprocess.check_output(cmd, shell=True)
-
-print output
-print ["\xe6\x9c\xba\xe6\x88\xbf"]
--- cmdb_excel/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/shuangjian/mygithub/homelab/cmdb_excel/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
