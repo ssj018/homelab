01e55318950634fdad8638c001b736f13bfe76ed
shuangjian <shuangjian@shuangjianvm.mds-trading.com>
2020-06-29 22:02:31 +0800

    update tool

--- network/code/tools/convert_ip_to_bytes.py
+++ network/code/tools/convert_ip_to_bytes.py
@@ -2,7 +2,9 @@ import struct
 
 def convert_ip_to_bytes(ip):
     sections = ip.split('.')
-    print(sections)
+    Bytes_IP = struct.pack('>4B', int(sections[0]), int(sections[1]), int(sections[2]), int(sections[3]))
+    return  Bytes_IP
 
 if __name__ == "__main__":
-    convert_ip_to_bytes('192.168.1.1')
+    b_ip = convert_ip_to_bytes('192.168.1.1')
+    print(b_ip)
